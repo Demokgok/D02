280a81c253bbf7816da01bc46ee99dcb078747f4
Deshi Mokgokong <demokgok@c5r5s5.wethinkcode.co.za>
2020-07-14 12:26:57 +0200

    pls check

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -6,29 +6,38 @@
 /*   By: demokgok <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 11:56:55 by demokgok          #+#    #+#             */
-/*   Updated: 2020/07/14 11:29:26 by demokgok         ###   ########.fr       */
+/*   Updated: 2020/07/14 12:24:20 by demokgok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void ft_putchar (char c)
 {
 	write(1, &c, 1);
 }
-void ft_putnbr(int nb)
+void ft_putnbr(int nbr)
 {
-	char* c_nb = (char)nb;
-	int i = 0;
-	while(c_nb[i] != '\0')
+	if (nbr < 0)
 	{
-		ft_putchar(c_nb[i]);
-		i++;
+		 ft_putchar(' -');
+	   nbr *= -1;
+	}
+	if (nbr < 10)
+{
+	ft_putchar(nbr + '0');
+}
+else
+{
+	ft_putnbr(nbr % 10);
+	ft_putnbr(nbr / 10);
+
 	}
 }
 
 int main()
 {
-	ft_putnbr(42);
+	ft_putnbr(256);
+	ft_putchar('\n');
 	return (0);
 }
